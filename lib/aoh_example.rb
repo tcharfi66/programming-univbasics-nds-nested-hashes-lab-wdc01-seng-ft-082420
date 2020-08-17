78da1eab9228f5e13c481bcb6c89fe71357516cf
Tej Charfi <charfi@usc.edu>
2020-08-17 17:35:39 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,7 +16,8 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  [[DON_G.sort], [JOELLE_VD.sort], [PAT_M.sort], [KATE_G.sort], [BRUCE_G.sort]]
+   x= [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+   x.sort
 end
 
 def aoh_lookup(aoh, row, key)
